4648faedc50e2503d04b8224d59dfb2da473add1
Salih Pehlivan <74909063+salihpehlivan@users.noreply.github.com>
2021-09-29 22:38:23 +0300

    flask-01-02-eklendi

---
+++ hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World from Flask!"
+
+@app.route('/second')
+def second():
+    return "Bize her yer Angara"
+
+@app.route('/third/subthird')
+def third():
+    return "This page is the subpage of third page"
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f"ID number of this page is {id}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=2000)
+
+
